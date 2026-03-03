1e89b3c71d53853d2b01bd9d56ad07a8d3473c99
Charles Pletcher <cpletcher2@gmail.com>
2026-03-03 11:33:37 -0500

    Update name

--- config/config.exs
+++ config/config.exs
@@ -2,10 +2,10 @@ import Config
 
 config :kodon,
   output_dir: "output",
-  site_title: "A Homeric Commentary in Progress",
+  site_title: "A Homer Commentary in Progress",
   commentary_dir: "commentary",
   commentary_dump_path: "my_commentary_pg_dump.db",
   templates_dir: Path.expand("../priv/templates", __DIR__),
   cross_ref_prefix: "I",
   cross_ref_default_slug: "tlg0012.tlg001",
-  url_prefix: "/ahcip"
+  url_prefix: ""
--- lib/ahcip.ex
+++ lib/ahcip.ex
@@ -1,6 +1,6 @@
 defmodule AHCIP do
   @moduledoc """
-  A Homeric Commentary in Progress.
+  A Homer Commentary in Progress.
 
   An interactive scholarly reading environment for Homer's Iliad, Odyssey,
   and the Homeric Hymns. Uses Kodon as its static site generator.
